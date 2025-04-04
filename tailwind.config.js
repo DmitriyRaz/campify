a36c4204b57fbfc70fcae01f6c3493546f76c99c
Dmitriy R. <razmyslovich.dmitriy@gmail.com>
2025-04-04 14:44:11 -0700

    tesing preview

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/domains/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/shared/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // You can define your color palette here
+      },
+    },
+  },
+  plugins: [],
+}
